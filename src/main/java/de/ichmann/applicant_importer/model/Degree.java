ae7faf6e0aae4b37d24da4eecd8348f69f0da7cf
Christian Wichmann <cw1981@gmx.de>
2015-03-22 16:18:42 +0100

    * Added some Javadoc comments.

--- src/main/java/de/ichmann/applicant_importer/model/Degree.java
+++ src/main/java/de/ichmann/applicant_importer/model/Degree.java
@@ -8,62 +8,62 @@ package de.ichmann.applicant_importer.model;
 public enum Degree {
 
     /**
-     * 
+     * Ohne erfolgreichen Besuch/Abschluss.
      */
     OHNE_ABSCHLUSS("OA"),
 
     /**
-     * 
+     * Hauptschulabschluss (einschl. qualifizierter).
      */
     HAUPTSCHULABSCHLUSS("HA"),
 
     /**
-     * 
+     * SEK I - Hauptschulabschl. / Hauptschulab. n. der 10. Klasse.
      */
     SEKUNDAR_I_HAUPTSCHULE("HK"),
 
     /**
-     * 
+     * Sekundarabschluss I - Realschulabschluss.
      */
     SEKUNDAR_I_REALSCHULE("SI"),
 
     /**
-     * 
+     * Erweiterter Sekundarabschluss I.
      */
     ERWEITERTER_SEKUNDAR_I("EI"),
 
     /**
-     * 
+     * Fachhochschulreife.
      */
     FACHHOCHSCHULREIFE("FH"),
 
     /**
-     * 
+     * Fachgebundene Hochschulreife.
      */
     FACHGEBUDENE_HOCHSCHULREIFE("GH"),
 
     /**
-     * 
+     * Schulischer Teil der Fachhochschulreife.
      */
     SCHULISCHER_TEIL_DER_FACHHOCHSCHULREIFE("FT"),
 
     /**
-     * 
+     * AH Allgemeine Hochschulreife.
      */
     ALLGEMEINE_HOCHSCHULEREIFE("AH"),
 
     /**
-     * 
+     * Abschluss der Förderschule Schwerpunkt Lernen.
      */
     ABSCHLUSS_DER_FOERDERSCHULE("AL"),
 
     /**
-     * 
+     * Sonst. ausländischer Schulabschluss.
      */
     SONSTIGER_AUSLAENDISCHER_ABSCHLUSS("XA"),
 
     /**
-     * 
+     * Sonst. Schulabschluss.
      */
     SONSTIGER_ABSCHLUSS("XS");
 
@@ -71,7 +71,7 @@ public enum Degree {
 
     /**
      * Instantiate a new enumeration value for type of degree.
-     * 
+     *
      * @param id
      *            id identifying the type of degree
      */
--- src/main/java/de/ichmann/applicant_importer/model/School.java
+++ src/main/java/de/ichmann/applicant_importer/model/School.java
@@ -2,163 +2,163 @@ package de.ichmann.applicant_importer.model;
 
 /**
  * Defines all school types and their id used in BBS-Planung to identify them.
- * 
+ *
  * @author Christian Wichmann
  */
 public enum School {
 
     /**
-     * 
+     * Berufseinstiegsklasse.
      */
     BERUFSEINSTIEGSKLASSE("BE"),
 
     /**
-     * 
+     * Berufsschule mit Teilzeit oder Blockunterricht.
      */
     BERUFSSCHULE("BS"),
 
     /**
-     * 
+     * Kooperatives Berufsgrundbildungsjahr.
      */
     KOOPERATIVES_BERUFSGRUNDBILDUNGSJAHR("BK"),
 
     /**
-     * 
+     * Schulisches Berufsgrundbildungsjahr.
      */
     SCHULISCHES_BERUFSGRUNDBILDUNGSJAHR("BG"),
 
     /**
-     * 
+     * Berufsoberschule.
      */
     BERUFSOBERSCHULE("BO"),
 
     /**
-     * 
+     * Berufsvorbereitungsjahr - Regelform.
      */
     BERUFSVORBEREITUNGSJAHR("BV"),
 
     /**
-     * 
+     * Berufsvorbereitungsjahr für Aussiedler und Ausländer.
      */
     BERUFSVORBEREITUNGSJAHR_FIER_AUSLAENDER("BR"),
 
     /**
-     * 
+     * Einjährige BFS.
      */
     BERUFSFACHSCHULE_EINJAEHRIG("B1"),
 
     /**
-     * 
+     * Einjährige BFS, SEK I - Abschluss - Realschulabschluss.
      */
     BERUFSFACHSCHULE_EINJAEHRIG_RS("B2"),
 
     /**
-     * 
+     * Einjährige oder eineinhalbj. BFS, berufl. Abschluss.
      */
     BERUFSFACHSCHULE_EINEINHALBJAEHRIG("B4"),
 
     /**
-     * 
+     * Zweijährige BFS, die zu einem beruflichen Abschluss führt.
      */
     BERUFSFACHSCHULE_ZWEIJAEHRIG("B7"),
 
     /**
-     * 
+     * Zweijährige BFS, die zu einem schulischen Abschluss führt.
      */
     BERUFSFACHSCHULE_ZWEIJAEHRIG_RS("B8"),
 
     /**
-     * 
+     * Einjährige oder eineinhalbjährige Fachschule.
      */
     FACHSCHULE_EINJAEHRIG("F1"),
 
     /**
-     * 
+     * Zweijährige Fachschule.
      */
     FACHSCHULE_ZWEIJAEHRIG("F2"),
 
     /**
-     * 
+     * Fachschule Seefahrt.
      */
     FACHSCHULE_SEEFAHRT("F4"),
 
     /**
-     * 
+     * Fachoberschule.
      */
     FACHOBERSCHULE("FO"),
 
     /**
-     * 
+     * Berufliches Gymnasium.
      */
     FACHGYMNASIUM("FG"),
 
     /**
-     * 
+     * Fachhochschule.
      */
     FACHHOCHSCHULE("FA"),
 
     /**
-     * 
+     * Freie Waldorfschule.
      */
     FREIE_WALDORFSCHULE("FW"),
 
     /**
-     * 
+     * Gymnasium Sekundarstufe I (bis einschließlich Klasse 9).
      */
     GYMNASIUM_BIS_KLASSE_9("G1"),
 
     /**
-     * 
+     * Gymnasium Sekundarstufe I (Klasse 10).
      */
     GYMNASIUM_BIS_KLASSE_10("G2"),
 
     /**
-     * 
+     * Gymnasiale Oberstufe.
      */
     GYMNASIUM_OBERSTUFE("GY"),
 
     /**
-     * 
+     * Hochschule.
      */
     HOCHSCHULE("HO"),
 
     /**
-     * 
+     * Hauptschule/ Hauptschulzweig.
      */
     HAUPTSCHULE("HS"),
 
     /**
-     * 
+     * Integrierte Gesamtschule.
      */
     GESAMTSCHULE("IG"),
 
     /**
-     * 
+     * Realschule/-zweig/Sekundarstufe I des Gymnasiums.
      */
     REALSCHULE("RS"),
 
     /**
-     * 
+     * Förderschule, mit Ausnahme Schwerpunkt Lernen.
      */
     FOERDERSCHULE("SA"),
 
     /**
-     * 
+     * Förderschule Schwerpunkt Lernen.
      */
     FOERDERSCHULE_SCHWERPUNKT_LERNEN("SL"),
 
     /**
-     * 
+     * Schule in den neuen Bundesländern.
      */
     SCHULE_IN_NEUEN_BUNDESLAENDERN("XD"),
 
     /**
-     * 
+     *
      */
     OBERSCHULE("OS"), // TODO Check if identifier is correct!
 
     /**
-     * 
+     * Sonstige Schule.
      */
     SONSTIGES("XS");
 
@@ -166,7 +166,7 @@ public enum School {
 
     /**
      * Instantiate a new enumeration value for a school type.
-     * 
+     *
      * @param id
      *            id identifying the last school type
      */
